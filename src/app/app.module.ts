7fa17eca7b36b3ab008432cd93b42504a2a5cc9a
nyiriistvan <92971467+nyiriistvan@users.noreply.github.com>
2023-02-07 16:42:02 +0100

    Komponensek létrehozása és az elérési út megadása a fejléc elemeire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { GithubComponent } from './github/github.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    GithubComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
